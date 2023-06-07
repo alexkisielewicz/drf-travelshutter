57e7143fd825c539ed60187cf5acf63af2146369
Aleksander Kisielewicz <aleksander.kisielewicz@gmail.com>
2023-06-07 19:41:01 +0000

    Add post categories as constant tuple

---
+++ posts/constants.py
@@ -0,0 +1,11 @@
+# categories for model Post
+POST_CATEGORIES = (
+        ('adventure', 'Adventure'),
+        ('travel', 'Travel'),
+        ('nature', 'Nature'),
+        ('landscape', 'Landscape'),
+        ('aerial', 'Aerial'),
+        ('wildlife', 'Wildlife'),
+        ('street', 'Street'),
+        ('architecture', 'Architecture'),
+    )
