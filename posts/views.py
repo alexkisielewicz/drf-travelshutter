0d5cb688b078c95baf82769115f8a0cee78f0139
Aleksander Kisielewicz <aleksander.kisielewicz@gmail.com>
2023-06-07 20:26:47 +0000

    Add PostList view

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from rest_framework import status
+from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Post
@@ -6,7 +6,21 @@ from .serializers import PostSerializer
 
 
 class PostList(APIView):
+    # render create post form
+    serializer_class = PostSerializer
+    # user has to be logged in in order to create posts
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+        
     def get(self, request):
         posts = Post.objects.all()
         serializer = PostSerializer(posts, many=True, context={'request': request})
         return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = PostSerializer(
+            data=request.data, context={"request": request}
+        )
+        if serializer.is_valid():
+            serializer.save(owner=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
