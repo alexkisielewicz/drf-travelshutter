749c9c49334c4aeacd3186d961f67bbcad92b795
Aleksander Kisielewicz <aleksander.kisielewicz@gmail.com>
2023-06-08 09:17:40 +0000

    Add generic list and detail view to likes app

--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_travelshutter.permissions import IsOwnerOrReadOnly
+from likes.models import Like
+from likes.serializers import LikeSerializer
 
-# Create your views here.
+
+class LikeList(generics.ListCreateAPIView):
+    """
+    List all likes or create a like if user is logged in.
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class LikeDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve a like or delete it by id if user owns it.
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
