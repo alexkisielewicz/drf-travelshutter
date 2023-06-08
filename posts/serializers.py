40fc24855cc18f2aa0fb830f348908d5113a748a
Aleksander Kisielewicz <aleksander.kisielewicz@gmail.com>
2023-06-08 12:43:51 +0000

    Add like id and comments/likes count to serializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Post
+from likes.models import Like
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -7,6 +8,9 @@ class PostSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source="owner.profile.id")
     profile_image = serializers.ReadOnlyField(source="profile.profile.image.url")
+    comments_count = serializers.ReadOnlyField()
+    like_id = serializers.SerializerMethodField()
+    likes_count = serializers.ReadOnlyField()
     
     def validate_image(self, value):
         if value.size > 1024 * 1024 * 2:
@@ -26,8 +30,17 @@ class PostSerializer(serializers.ModelSerializer):
     
     def get_is_owner(self, obj):
         # check post ownership
-        request = self.context['request']
+        request = self.context["request"]
         return request.user == obj.owner
+    
+    def get_like_id(self, obj):
+        user = self.context["request"].user
+        if user.is_authenticated:
+            like = Like.objects.filter(
+                owner=user, post=obj
+            ).first()
+            return like.id if like else None
+        return None
     
     class Meta:
         model = Post
@@ -44,4 +57,7 @@ class PostSerializer(serializers.ModelSerializer):
             "exif",
             "body",
             "image",
+            'like_id',
+            'comments_count',
+            'likes_count',
         ]
--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,8 @@
-from django.http import Http404
-from rest_framework import generics, permissions
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics, permissions, filters
 from .models import Post
 from .serializers import PostSerializer
 from drf_travelshutter.permissions import IsOwnerOrReadOnly
+from django.db.models import Count
 
 
 class PostList(generics.ListCreateAPIView):
@@ -12,7 +10,26 @@ class PostList(generics.ListCreateAPIView):
     serializer_class = PostSerializer
     # user has to be logged in in order to create posts
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    queryset = Post.objects.all()
+    queryset = Post.objects.annotate(
+        comments_count=Count(
+            'comment',
+            distinct=True
+        ),
+        likes_count=Count(
+            'likes',
+            distinct=True
+        )
+    ).order_by('-created_at')
+    filter_backends = [
+        filters.OrderingFilter,
+        # filters.SearchFilter,
+        # DjangoFilterBackend,
+    ]
+    ordering_fields = [
+        'comments_number',
+        'likes_number',
+        'likes__created_at',
+    ]
     
     def create_post(self, serializer):
         # method to assign owner to the post before save
@@ -23,4 +40,13 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     # check if requesting user is post owner
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = PostSerializer
-    queryset = Post.objects.all()
+    queryset = Post.objects.annotate(
+        comments_count=Count(
+            'comment',
+            distinct=True
+        ),
+        likes_count=Count(
+            'likes',
+            distinct=True
+        )
+    ).order_by('-created_at')
