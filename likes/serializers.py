22b5c1f0f3d225de9e44bcf2d62edbf1af53bd1b
Aleksander Kisielewicz <aleksander.kisielewicz@gmail.com>
2023-06-08 09:16:26 +0000

    Add serializer and url to likes app

---
+++ likes/serializers.py
@@ -0,0 +1,30 @@
+from django.db import IntegrityError
+from rest_framework import serializers
+from .models import Like
+
+
+class LikeSerializer(serializers.ModelSerializer):
+    """
+    Class for LikeSerializer
+    """
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Like
+        fields = [
+            'id',
+            'owner',
+            'created_at',
+            'post',
+        ]
+
+    def create(self, validated_data):
+        """
+        Method to raise an error if user like the same post twice
+        """
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicate'
+            })
