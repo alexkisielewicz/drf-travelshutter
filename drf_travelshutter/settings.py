58c5b959774c5aac604dfa7f52b39781308167e8
Aleksander Kisielewicz <aleksander.kisielewicz@gmail.com>
2023-06-08 15:53:20 +0000

    Add JSON Web Tokens to the project

--- drf_travelshutter/settings.py
+++ drf_travelshutter/settings.py
@@ -26,6 +26,25 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [(
+        'rest_framework.authentication.SessionAuthentication'
+        if 'DEV' in os.environ
+        else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
+    )]
+}
+
+REST_USE_JWT = True
+JWT_AUTH_SECURE = True
+JWT_AUTH_COOKIE = 'travelshutter-auth'
+JWT_AUTH_REFRESH_COOKIE = 'travelshutter-refresh'
+JWT_AUTH_SAMESITE = 'None'
+
+REST_AUTH_SERIALIZERS = {
+    'USER_DETAILS_SERIALIZER': 'drf_travelshutter.serializers.CurrentUserSerializer'
+}
+
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
