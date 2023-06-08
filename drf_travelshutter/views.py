8b329e5d89979f25852bd48e85eecf03cda4f184
Aleksander Kisielewicz <aleksander.kisielewicz@gmail.com>
2023-06-08 16:15:31 +0000

    Edit timestamp format

--- drf_travelshutter/views.py
+++ drf_travelshutter/views.py
@@ -1,13 +1,16 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-
 class RootView(APIView):
     def get(self, request):
         data = {
             'message': 'Welcome to TravelShutter API!',
             'endpoints': {
                 'profiles': '/profiles',
+                'posts': '/posts',
+                'comments': '/comments',
+                'likes': '/likes',
+                'followers': '/followers',
             }
         }
         return Response(data)
